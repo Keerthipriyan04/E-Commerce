eadb56fc2f82c33fbb3ef2dedc1f5b28edfc741f
skvpriyan@gmail.com <skvpriyan@gmail.com>
2024-01-25 12:29:28 +0530

    e-commerce website project half frontend

---
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './NewsLetter.css'
+
+const NewsLetter = () => {
+  return (
+    <div className='newsletter'>
+        <h1>GET EXCLUSIVE 
+            OFFERS ON YOUR EMAIL</h1>
+        <p>Subscribe to our newletter and stay updated</p>
+        <div>
+            <input type="email" placeholder="your email id "/> 
+            <button>Subscribe</button>   
+        </div>
+
+    </div>
+  )
+}
+
+export default NewsLetter
